bd1d1e362ffe3748db3bfa1f2261adf536773309
Ji998 <xij1@hi.is>
2026-03-18 19:41:14 +0000

    laga CORS

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { authorsRouter } from './routes/authors.js'
 import { articlesRouter } from './routes/articles.js'
 
 export const app = new Hono()
+
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:5173'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type'],
+  }),
+)
+
 
 app.get('/', (c) => {
   return c.json({
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import { serve } from '@hono/node-server'
 import { app } from './app.js'
-import { cors } from 'hono/cors'
 
-app.use(
-  '*',
-  cors({
-    origin: ['http://localhost:5173'],
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowHeaders: ['Content-Type'],
-  }),
-)
 
 const port = 3000
 
